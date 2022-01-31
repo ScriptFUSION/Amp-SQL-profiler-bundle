12b0546d4c6bbbf80196e08486210631aa2497bc
Bilge <bilge@scriptfusion.com>
2022-01-31 17:21:43 +0000

    Added backtrace support to Amp SQL profiler.

--- src/SqlProfiler/SqlQuery.php
+++ src/SqlProfiler/SqlQuery.php
@@ -7,10 +7,13 @@ use Symfony\Component\VarDumper\Cloner\Data;
 
 final class SqlQuery
 {
+    public array $backtrace;
+
     public function __construct(
         public string $sql,
         public float $executionMS,
         public array|Data $params = [],
     ) {
+        $this->backtrace = array_slice(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), 1);
     }
 }
