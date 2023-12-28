61d8e7d11fe3601beaef5b48964d59d907902833
Bilge <bilge@scriptfusion.com>
2023-12-28 23:52:58 +0000

    Added Symfony bundle class. Moved source files to correct directory structure.

---
+++ src/AmpSqlProfilerBundle.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace ScriptFUSION\AmpSqlProfilerBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+final class AmpSqlProfilerBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import(__DIR__ . '/../config/services.yaml');
+    }
+}
